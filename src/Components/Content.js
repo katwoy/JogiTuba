c5c44c8968a50e76edb5879fcf220815414c3a77
Katarzyna Wojcicka <katwoy@gmail.com>
2020-03-04 20:27:11 +0100

    Add Content component

---
+++ src/Components/Content.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchForm from './SearchForm';
+import VideoDetail from './VideoDetail';
+import VideoList from './VideoList';
+
+const Content = () => {
+  return (
+    <>
+      <SearchForm />
+      <VideoDetail />
+      <VideoList />
+    </>
+  )
+}
+
+export default Content;
