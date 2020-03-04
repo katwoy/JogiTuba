475c12b13f7d3825671d8034e6f6543f6473d8fc
Katarzyna Wojcicka <katwoy@gmail.com>
2020-03-04 20:27:30 +0100

    Add React Router

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Start from './Start';
-import SearchForm from './SearchForm';
-import VideoDetail from './VideoDetail';
-import VideoList from './VideoList';
+import Content from './Content';
+import {
+  HashRouter,
+  Route,
+  Switch,
+  } from 'react-router-dom';
 
 const Main = () => {
   return (
     <main className="page-main">
       <div className="main-container">
+        <HashRouter>
+          <Switch>
+          <Route exact path='/' component={Start} />
+          <Route path='/content' component={Content} />
+          </Switch>
+        </HashRouter>
         <Start />
-        <SearchForm />
-        <VideoDetail />
-        <VideoList />
+        <Content />
       </div>
     </main>
   )
--- src/Components/Start.js
+++ src/Components/Start.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Start = () => {
   return (
     <div className="welcome-message">
       <h2 className="welcome-message-title">Witaj na JogiTubie!</h2>
       <div className="welcome-message-text">Jesteś w idealnym miejscu, aby zacząć swoją domową praktykę. Znajdziesz tu filmy z jogą i medytacjami od nauczycielek z najlepszych polskich kanałów YouTube. Możesz praktykować o dowolnej porze i w dowolnym miejscu. Żeby zacząć, potrzebujesz jedynie kawałka wolnej przestrzeni.</div>
-      <button className="btn-without-log btn-main">Zacznij teraz</button>
+      <Link to="/content"><button className="btn-without-log btn-main">Zacznij teraz</button></Link>
     </div>
   )
 }
