8201437b5ac470a00d60dfd7af98d17ea77588b6
Katarzyna Wojcicka <katwoy@gmail.com>
2020-03-12 14:59:03 +0100

    Change arrangement of elements in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,13 +5,11 @@ const Footer = () => {
   return (
     <footer className="page-footer">
       <div className="footer-container">
-        <div className="footer-attributions">
-          <div className="footer-copyright">
-            Copyright © 2020 <strong>JogiTuba</strong>
-          </div>
-          <div className="footer-img-attribution">
-            Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" rel="nofollow">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/" rel="nofollow">www.flaticon.com</a>
-          </div>
+        <div className="footer-copyright">
+          Copyright © 2020 <strong>JogiTuba</strong>
+        </div>
+        <div className="footer-icon-attribution">
+          Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" rel="nofollow">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/" rel="nofollow">www.flaticon.com</a>
         </div>
       </div>
     </footer>
