ae2c0b90ecab0b6fa9659cc6377d45148f1793b1
Katarzyna Wojcicka <katwoy@gmail.com>
2020-03-12 17:14:16 +0100

    Modify Start component

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -6,8 +6,8 @@ const Start = () => {
   return (
     <div className="welcome-message">
       <h2 className="welcome-message-title">Witaj na JogiTubie!</h2>
-      <div className="welcome-message-text">Jesteś w idealnym miejscu, aby zacząć swoją domową praktykę. Znajdziesz tu filmy z jogą i medytacjami od nauczycielek z najlepszych polskich kanałów YouTube. Możesz praktykować o dowolnej porze i w dowolnym miejscu. Żeby zacząć, potrzebujesz jedynie kawałka wolnej przestrzeni.</div>
-      <Link to="/content"><button className="btn-without-log btn-main">Zacznij teraz</button></Link>
+      <div className="welcome-message-text">Jesteś w idealnym miejscu, aby zacząć swoją domową praktykę jogi i medytacji. Znajdziesz tu filmy instruktażowe od nauczycielek z najlepszych polskich kanałów YouTube. Możesz praktykować o dowolnej porze i w dowolnym miejscu. Żeby zacząć, potrzebujesz jedynie kawałka wolnej przestrzeni.</div>
+      <Link to="/content"><button className="btn">Zacznij teraz</button></Link>
     </div>
   )
 }
