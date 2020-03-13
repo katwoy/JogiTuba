2b847bbf6de4aec6682c3b820824effca904611c
Katarzyna Wojcicka <katwoy@gmail.com>
2020-03-13 20:34:27 +0100

    Change the way of video display

--- src/Components/VideoDetail.js
+++ src/Components/VideoDetail.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './VideoDetail.css';
 
-const VideoDetail = ({video}) => {
+const VideoDetail = ({video, handleClose}) => {
 
   const url = `https://www.youtube.com/embed/${video}`;
 
   return (
     <div className="video-display">
-      <iframe title="YouTube video" src={url} width="640" height="360" allowFullScreen frameBorder="0"></iframe>
+      <div className="video-display-shade"></div>
+      <div className="video-display-container">
+        <iframe title="YouTube video" src={url} width="640" height="360" allowFullScreen frameBorder="0"></iframe>
+        <button className="video-display-close" onClick={() => handleClose(false)}>X</button>
+      </div>
     </div>
   )
 
--- src/Components/VideoList.js
+++ src/Components/VideoList.js
@@ -6,23 +6,29 @@ import VideoDetail from './VideoDetail';
 const VideoList = ({videos}) => {
 
   const [selectedVideo, setSelectedVideo] = useState("");
+  const [videoVisibility, setVideoVisibility] = useState(false);
 
   const handleVideoSelect = (video) => {
     setSelectedVideo(video)
+    setVideoVisibility(true)
+  }
+
+  const handleClose = (visibility) => {
+    setVideoVisibility(visibility)
   }
 
   const videoItems = videos.map(video => {
     return (
       <>
-        <VideoListItem key={video.contentDetails.videoId} video={video} handleVideoSelect={handleVideoSelect}/>   
+        <VideoListItem key={video.contentDetails.videoId} video={video} handleVideoSelect={handleVideoSelect} />   
       </>    
     )
   })
 
   return (  
     <>
-      {selectedVideo !== "" && <VideoDetail video={selectedVideo}/>}
-      <h3 className="video-list-title">{selectedVideo === "" ? "Kliknij w miniaturę, aby wyświetlić film" : "Kliknij w miniaturę, aby zmienić wyświetlany film"}</h3>
+      {videoVisibility && <VideoDetail video={selectedVideo} handleClose={handleClose}/>}
+      <h3 className="video-list-title">Kliknij w miniaturę, aby wyświetlić film</h3>
       <ul className="video-list">
         {videoItems}
       </ul>
