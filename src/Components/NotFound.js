658b9abc0d53dc582e2529a65b6d031257b72fd8
Katarzyna Wojcicka <katwoy@gmail.com>
2020-03-14 16:41:56 +0100

    Make NotFound responsive and add tooltip to savasana image

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 import './NotFound.css';
 import savasana from '../img/savasana.png';
 
 const NotFound = () => {
+
+  const [tooltip, setTooltip] = useState("hidden");
+
+  const handleMouseEnter = () => {
+    setTooltip("visible")
+  }
+
+  const handleMouseLeave = () => {
+    setTooltip("hidden")
+  }
+
   return (
     <div className="error-404">
       <h1 className="error-404-title">Ups... Nie ma strony o takim adresie.</h1>
@@ -12,7 +23,8 @@ const NotFound = () => {
       <div className="error-404-text">lub</div>
       <Link to="/content" className="error-404-link">Przejdź do wyboru nauczycielki</Link>
       <div className="error-404-text">lub</div>
-      <img src={savasana} alt="savasana" className="error-404-img"/>
+      <img src={savasana} alt="savasana" className="error-404-img" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}/>
+      <div className="error-404-text" style={{visibility: tooltip}}>Zrelaksuj się w savasanie. To zawsze dobry pomysł :)</div>
     </div>
   )
 }
