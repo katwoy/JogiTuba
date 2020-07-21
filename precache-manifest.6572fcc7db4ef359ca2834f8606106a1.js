dfa20cb618401e17eebdda64d3281049e18e0673
Katarzyna Wojcicka <katwoy@gmail.com>
2020-07-21 19:22:41 +0200

    Updates

---
+++ precache-manifest.6572fcc7db4ef359ca2834f8606106a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e932e5c2bea7e783b1798dc933a1951",
+    "url": "/JogiTuba/index.html"
+  },
+  {
+    "revision": "42a1739426adf76b7284",
+    "url": "/JogiTuba/static/css/main.f84e894b.chunk.css"
+  },
+  {
+    "revision": "9ba0c8005a1746aa40a8",
+    "url": "/JogiTuba/static/js/2.65a17b4d.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/JogiTuba/static/js/2.65a17b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a1739426adf76b7284",
+    "url": "/JogiTuba/static/js/main.3a5b1d6d.chunk.js"
+  },
+  {
+    "revision": "708df44cda823eac5899",
+    "url": "/JogiTuba/static/js/runtime-main.3b22d269.js"
+  },
+  {
+    "revision": "c509e52544f2b62ec8ee813c2e129432",
+    "url": "/JogiTuba/static/media/logo.c509e525.png"
+  },
+  {
+    "revision": "32b74098b12be834c1e158789198f48f",
+    "url": "/JogiTuba/static/media/savasana.32b74098.png"
+  },
+  {
+    "revision": "b05c776c05a9f8d57906b8578923e955",
+    "url": "/JogiTuba/static/media/teacher.b05c776c.png"
+  },
+  {
+    "revision": "36c46a1c884dd51ddf47dfa81ec40587",
+    "url": "/JogiTuba/static/media/warrior.36c46a1c.png"
+  }
+]);
