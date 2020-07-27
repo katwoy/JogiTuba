353b6e908d469fe7f83bd4916c9d39c15a1ef3ca
Katarzyna Wojcicka <katwoy@gmail.com>
2020-07-27 19:38:17 +0200

    Change home path in logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Header.css';
 import logo from '../img/logo.png';
+import {HashRouter, Link} from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="page-header">
-      <a href="/" className="header-logo">
-        <img src={logo} alt="yogi" className="header-logo-image"/>
-        <h1 className="header-logo-title">JogiTuba</h1>
-      </a>
+      <HashRouter>
+        <Link to="/" className="header-logo">
+          <img src={logo} alt="yogi" className="header-logo-image"/>
+          <h1 className="header-logo-title">JogiTuba</h1>
+        </Link>
+      </HashRouter>
     </header>
   )
 }
