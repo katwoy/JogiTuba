1b1ab8597b6fb6c03242eb456745e1c540c29ea2
Katarzyna Wojcicka <katwoy@gmail.com>
2020-07-27 19:51:16 +0200

    Change home path in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Footer.css';
+import {HashRouter, Link} from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className="page-footer">
       <div className="footer-copyright">
-        Copyright © 2020 <a href="/">JogiTuba</a>
+        <HashRouter>
+          Copyright © 2020 <Link to="/">JogiTuba</Link>
+        </HashRouter>
       </div>
       <div className="footer-icon-attribution">
         Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" rel="nofollow">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/" rel="nofollow">www.flaticon.com</a>
